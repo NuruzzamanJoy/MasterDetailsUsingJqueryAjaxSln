56e428239fd9b7f9824d1c085c7699f52435eb1d
SCSL60 <SCSL60@DESKTOP-AQJIFQQ>
2024-04-22 12:57:04 +0600

    Add project files.

---
+++ MasterDetailsUsingJqueryAjax/Models/Experiance.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MasterDetailsUsingJqueryAjax.Models
+{
+    public class Experiance
+    {
+        [Key]
+        public int ExperianceId { get; set; }
+        [ForeignKey("Applicant")]
+        public int  ApplicantId { get; set; }
+        public Applicant Applicant { get; private set; }
+        public string CompanyName { get; set; }       
+        public int YearsWorked { get; set; }
+
+    }
+}
